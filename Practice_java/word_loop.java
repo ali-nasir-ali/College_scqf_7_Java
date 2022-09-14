9663da4870ce9111dfaffdeab6d382978373af9b
nasir ali <a.nasirali@outlook.com>
2022-09-14 19:34:00 +0100

    Create word_loop.java

---
+++ Practice_java/word_loop.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("loops of java");
+     System.out.println("\n this is While loop");
+     int num = 1;
+    while(num<=5){
+      System.out.println(num);
+      num++;
+    }
+     System.out.println("\n this is DO While loop");
+     int numb = 1;
+     do{
+      System.out.println(numb);
+      numb++;
+    }while(numb<=5);
+     System.out.println("\n this is FOR loops of java");
+
+    for(int counter = 10;counter>0;counter--){
+      System.out.println(counter);
+    }
+    System.out.println("\n Now we have left the loop structure");
+  }
+}
