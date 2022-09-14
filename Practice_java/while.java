be4cecf34dff30640b1638731eaee9d1778749fc
nasir ali <a.nasirali@outlook.com>
2022-09-14 19:37:14 +0100

    Create while.java

---
+++ Practice_java/while.java
@@ -0,0 +1,19 @@
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("loops of java");
+
+     int num = 1;
+    while(num<=5){
+      System.out.println(num);
+      num++;
+    }
+
+     System.out.println("this is FOR loops of java");
+
+    for(int counter = 10;counter>0;counter--){
+      System.out.println(counter);
+    }
+  }
+}
